4187f7a7739dace0ee0de61b82caee1f10c56dd9
camiloibanez <camiloibanez8@gmail.com>
2021-07-16 09:24:34 -0500

    init

---
+++ EmployeeManagementSystem.java
@@ -0,0 +1,10 @@
+package com.cognixia.JUMP.intermediateJava.employeemanagementsystem;
+
+public class EmployeeManagementSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
