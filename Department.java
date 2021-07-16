6434720d618d5f7102deadfd404423fa6044710a
camiloibanez <camiloibanez8@gmail.com>
2021-07-16 09:40:23 -0500

    employee class set up

--- Department.java
+++ Department.java
@@ -1,5 +1,7 @@
 package com.cognixia.JUMP.intermediateJava.employeemanagementsystem;
 
 public enum Department {
-	
+
+	MARKETING, HUMAN_RESOURCES, PRODUCTION, RESEARCH_AND_DEVELOPMENT, PURCHASING,
+	ACCOUNTING, SALES, OPERATIONS, MANAGEMENT
 }
--- Employee.java
+++ Employee.java
@@ -2,4 +2,59 @@ package com.cognixia.JUMP.intermediateJava.employeemanagementsystem;
 
 public class Employee {
 
+	private Department department;
+	private int Salary;
+	private String firstName;
+	private String lastName;
+	private int id;
+	
+	public Employee(Department department, int salary, String firstName, String lastName, int id) {
+		super();
+		this.department = department;
+		Salary = salary;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public int getSalary() {
+		return Salary;
+	}
+
+	public void setSalary(int salary) {
+		Salary = salary;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}	
+	
 }
